3e0967afa997b0f6c650df7334c88b6242e1c258
FabricaSoftware120 <fabrica.software@newsol.com.co>
2025-01-21 12:30:17 -0500

    fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,15 +10,16 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://github.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/documentation-projects/",
+  deploymentBranch: "gh-pages",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "NewSol", // Usually your GitHub org/user name.
+  projectName: "documentation-projects", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
